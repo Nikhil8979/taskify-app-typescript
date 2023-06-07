b732c97439f4a21c7ba675d04df6e4124a72af56
Nikhil Singh <nikhil@aresourcepool.com>
2023-06-07 22:12:50 +0530

    todo crud finished

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -1,32 +1,58 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Todo } from "../types";
 import { AiFillEdit, AiFillDelete } from "react-icons/ai";
 import { MdDone } from "react-icons/md";
 type SingleTodoProps = {
   todo: Todo;
-  todos: Todo[];
-  setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
-  updateTaskCompleteHandler: (id: number) => void;
+  onUpdateTaskComplete: (id: number) => void;
+  onDeleteTask: (id: number) => void;
+  onEditTask: (e: React.FormEvent, id: number, editTodo: string) => void;
 };
 const SingleTodo: React.FC<SingleTodoProps> = ({
   todo,
-  setTodos,
-  todos,
-  updateTaskCompleteHandler,
+  onUpdateTaskComplete,
+  onDeleteTask,
+  onEditTask,
 }) => {
+  const [edit, setEdit] = useState<boolean>(false);
+  const [editTodo, setEditTodo] = useState<string>(todo.todo);
+
   return (
-    <form key={todo.id} className="todos__single">
-      <span className="todos__single--text">{todo?.todo}</span>
+    <form
+      key={todo.id}
+      onSubmit={(e) => onEditTask.bind(null, e, todo.id, editTodo)}
+      className="todos__single"
+    >
+      {edit ? (
+        <>
+          <input
+            value={editTodo}
+            onChange={(e) => setEditTodo(e.target.value)}
+            className="todos__single--text"
+          />
+        </>
+      ) : todo.isDone ? (
+        <s className="todos__single--text">{todo?.todo}</s>
+      ) : (
+        <span className="todos__single--text">{todo?.todo}</span>
+      )}
+
       <div>
         <span className="icon">
-          <AiFillEdit />
+          <AiFillEdit
+            onClick={() => {
+              if (!edit && !todo.isDone) {
+                setEdit(!edit);
+              }
+            }}
+          />
         </span>
         <span className="icon">
-          <AiFillDelete />
+          <AiFillDelete onClick={onDeleteTask.bind(null, todo.id)} />
         </span>
         <span
           className="icon"
-          onClick={updateTaskCompleteHandler.bind(null, todo.id)}
+          onClick={onUpdateTaskComplete.bind(null, todo.id)}
         >
           <MdDone />
         </span>
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -7,15 +7,40 @@ interface TodoListProps {
   setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
 }
 const TodoList: React.FC<TodoListProps> = ({ todos, setTodos }) => {
-  const updateTaskCompleteHandler = (id: number) => {};
+  const updateTaskCompleteHandler = (id: number) => {
+    setTodos((todos) => {
+      const todoArr: Todo[] = todos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, isDone: !todo.isDone };
+        }
+        return todo;
+      });
+      return todoArr;
+    });
+  };
+  const taskDeleteHandler = (id: number) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+  const taskEditHandler = (
+    event: React.FormEvent,
+    id: number,
+    editTodo: string
+  ) => {
+    event.preventDefault();
+    setTodos(
+      todos.map((todo) => {
+        return todo.id === id ? { ...todo, todo: editTodo } : todo;
+      })
+    );
+  };
   return (
     <div className="todos">
       {todos?.map((todo) => (
         <SingleTodo
-          updateTaskCompleteHandler={updateTaskCompleteHandler}
+          onEditTask={taskEditHandler}
+          onUpdateTaskComplete={updateTaskCompleteHandler}
+          onDeleteTask={taskDeleteHandler}
           todo={todo}
-          todos={todos}
-          setTodos={setTodos}
         />
       ))}
     </div>
